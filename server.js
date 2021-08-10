84a7b0f5ef9b2dda0b27b66b9c7024397037a119
unknown <akhilice13@gmail.com>
2021-08-10 17:21:01 +0530

    Created Cluster

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 import express from 'express'
+import os from "os"
+import cluster from 'cluster'
+
 const app = express()
 const router = express.Router()
 
 app.use(express.json())
 const PORT = 5000
 
-const testingRouter = router.get('/', (req, res) => {
-  res.send('Tested')
-})
+const numCpu = os.cpus().length
 
-app.use(testingRouter)
+app.use('/', (req, res) => {
+    
+    for (let i = 0; i < 1e8; i++) {
+      
+     }
+    res.send(`Tested... ${process.pid}`)
+    // cluster.worker.kill()
+  })
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`))
+if(cluster.isMaster){
+    for (let i = 0; i < numCpu; i++) {
+        cluster.fork()
+     }
+    //  cluster.on("exit", (worker, code, signal) =>{
+    //      console.log(`worker ${worker.process.pid} died`)
+    //      cluster.fork()
+    //  })
+    
+}else{
+    app.listen(PORT, console.log(`Server running on port ${PORT} && ${process.pid}`))
+}
+
+
+// app.listen(PORT, console.log(`Server running on port ${PORT}`))
+
+
+
